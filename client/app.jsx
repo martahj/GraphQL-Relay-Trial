e7fc805b17c4e00341c3e8a00b45ee8c4ebdf2a8
Marta Johnson <marta.johnson@Marta-Johnsons-MacBook-Pro.local>
2016-08-15 23:00:08 -0500

    babel for relay in place

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import Relay from 'react-relay';
 // import Pokemon from './components/pokemon';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
+    console.log('app props', this.props);
     this.state = {pokemonId: 1};
     this.changePokemon = this.changePokemon.bind(this);
   }
@@ -25,4 +26,16 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Relay.createContainer(App, {
+  initialVariables: {
+    id: this.state.pokemonId
+  },
+  fragments: {
+    pokemon: () => Relay.QL`
+    fragment on Pokemon() {
+      id
+      name
+    }
+    `,
+  },
+})
--- schema/schemaHelpers.js
+++ schema/schemaHelpers.js
@@ -2,7 +2,5 @@ import dataFetchers from './hardcodedData';
 
 export default {
   getPokemon: (id) => dataFetchers.findPokemonById(id),
-  getElement: (id) => dataFetchers.findElementById(id),
-  // getPreviousEvolution: (id) => getPreviousEvolution(id),
-  // getNextEvolution: (id) => getNextEvolution(id)
+  getElement: (id) => dataFetchers.findElementById(id)
 }
